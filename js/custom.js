63def5c16b630aa9322f320e13152b4ea0108e09
Mohamed Mostafa <mmostafa261@gmail.com>
2022-03-26 23:00:13 +0200

    First commit

---
+++ js/custom.js
@@ -0,0 +1,64 @@
+(function($) {
+    "use strict";
+	
+	$(window).on('load', function() {
+		$('.preloader').fadeOut();
+		$('#preloader').delay(550).fadeOut('slow');
+		$('body').delay(450).css({
+			'overflow': 'visible'
+		});
+	});
+	$(window).on('scroll', function() {
+		if ($(window).scrollTop() > 50) {
+			$('.main-header').addClass('fixed-menu');
+		} else {
+			$('.main-header').removeClass('fixed-menu');
+		}
+	});
+
+
+	$(document).ready(function() {
+		$(window).on('scroll', function() {
+			if ($(this).scrollTop() > 100) {
+				$('#back-to-top').fadeIn();
+			} else {
+				$('#back-to-top').fadeOut();
+			}
+		});
+		$('#back-to-top').click(function() {
+			$("html, body").animate({
+				scrollTop: 0
+			}, 600);
+			return false;
+		});
+	});
+
+	var Container = $('.container');
+	Container.imagesLoaded(function() {
+		var $grid = $('.special-list').isotope({
+			itemSelector: '.special-grid'
+		});
+	});
+	$(document).ready(function() {
+		$('[data-toggle="tooltip"]').tooltip();
+	});
+	
+	
+	$(document).ready(function() {
+		$(window).on('scroll', function() {
+			if ($(this).scrollTop() > 100) {
+				$('#back-to-top').fadeIn();
+			} else {
+				$('#back-to-top').fadeOut();
+			}
+		});
+		$('#back-to-top').click(function() {
+			$("html, body").animate({
+				scrollTop: 0
+			}, 600);
+			return false;
+		});
+	});
+	
+	
+}(jQuery));
